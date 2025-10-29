61bdb237e54e92c5aaba2f376b01695a8ebc465a
Giogro <giogro2000@gmail.com>
2025-10-29 19:03:54 +0100

    version 1.0

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sympy
 from sympy import symbols, Eq, solve, pi
 
 # CONSTANTS
-num_format = "%.2f"
+num_format = "%.2e"
 eq_keyset = [
     "p_r",
     "p_t",
@@ -40,25 +40,29 @@ def reset_state():
         st.session_state[symbol] = None
     for eq in eq_set:
         st.session_state[eq] = eq_set[eq]
+    st.session_state['steps'] = []
 
 
 def is_disabled(widget_key):
     return st.session_state[widget_key] != None
 
 
-def react(symbol, key):
-    value = st.session_state[key]
-    if value is None : return
+def react(symbol):
+    value = st.session_state[symbol.name]
+    if value is None:
+        return
     for eq_key in eq_set:
         free_syms = st.session_state[eq_key].free_symbols
         if symbol in free_syms:
             st.session_state[eq_key] = st.session_state[eq_key].subs(symbol, value)
             free_syms = st.session_state[eq_key].free_symbols
-            if(len(free_syms) == 1) : 
+            if len(free_syms) == 1:
                 solvend = next(iter(free_syms))
                 solution = solve(st.session_state[eq_key], solvend)[0]
                 st.session_state[solvend.name] = solution
-                react(solvend, solvend.name)
+                st.session_state['steps'].append(f"* Solved for **{solvend.name}** = `{solution:.2e}` (using `{eq_set[eq_key]}`)")
+                react(solvend)
+
 
 # INITIALIZATION
 if "p_r" not in st.session_state:
@@ -79,11 +83,16 @@ with col_sys1:
         format=num_format,
         disabled=is_disabled("freq"),
         on_change=react,
-        kwargs={"symbol": freq, "key": "freq"},
+        kwargs={"symbol": freq},
     )
 with col_sys2:
     st.number_input(
-        "Wavelength (m)", key="wvlen", format=num_format, disabled=is_disabled("wvlen")
+        "Wavelength (m)",
+        key="wvlen",
+        format=num_format,
+        disabled=is_disabled("wvlen"),
+        on_change=react,
+        kwargs={"symbol": wvlen},
     )
 
 st.divider()
@@ -93,22 +102,36 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.subheader("Transmitter")
     st.number_input(
-        "Tx Power, $P_t$ (W)", key="p_t", format=num_format, disabled=is_disabled("p_t")
+        "Tx Power, $P_t$ (W)",
+        key="p_t",
+        format=num_format,
+        disabled=is_disabled("p_t"),
+        on_change=react,
+        kwargs={"symbol": p_t},
     )
     st.number_input(
         "Tx Gain, $G_t$ (ratio)",
         key="g_t",
         format=num_format,
         disabled=is_disabled("g_t"),
+        on_change=react,
+        kwargs={"symbol": g_t},
     )
     st.number_input(
-        "Tx Loss Factor, $f_t$ (ratio)",
+        "Normalized $f_t$",
         key="f_t",
         format=num_format,
         disabled=is_disabled("f_t"),
+        on_change=react,
+        kwargs={"symbol": f_t},
     )
     st.number_input(
-        "EIRP (W)", key="eirp", format=num_format, disabled=is_disabled("eirp")
+        "EIRP (W)",
+        key="eirp",
+        format=num_format,
+        disabled=is_disabled("eirp"),
+        on_change=react,
+        kwargs={"symbol": eirp},
     )
 
 with col2:
@@ -118,47 +141,67 @@ with col2:
         key="radius",
         format=num_format,
         disabled=is_disabled("radius"),
+        on_change=react,
+        kwargs={"symbol": radius},
     )
     st.number_input(
-        "Path Loss, $FSL$ (ratio)",
+        "Free Space Loss, $FSL$",
         key="fsl",
         format=num_format,
         disabled=is_disabled("fsl"),
+        on_change=react,
+        kwargs={"symbol": fsl},
     )
     st.number_input(
         "Power Density, $S_r$ (W/m²)",
         key="s_r",
         format=num_format,
         disabled=is_disabled("s_r"),
+        on_change=react,
+        kwargs={"symbol": s_r},
     )
 
 with col3:
     st.subheader("Receiver")
     st.number_input(
-        "Rx Power, $P_r$ (W)", key="p_r", format=num_format, disabled=is_disabled("p_r")
+        "Rx Power, $P_r$ (W)",
+        key="p_r",
+        format=num_format,
+        disabled=is_disabled("p_r"),
+        on_change=react,
+        kwargs={"symbol": p_r},
     )
     st.number_input(
-        "Rx Gain, $G_r$ (ratio)",
+        "Rx Gain, $G_r$",
         key="g_r",
         format=num_format,
         disabled=is_disabled("g_r"),
+        on_change=react,
+        kwargs={"symbol": g_r},
     )
     st.number_input(
-        "Rx Loss Factor, $f_r$ (ratio)",
+        "Normalized $f_r$",
         key="f_r",
         format=num_format,
         disabled=is_disabled("f_r"),
+        on_change=react,
+        kwargs={"symbol": f_r},
     )
     st.number_input(
         "Effective Aperture, $A_e$ (m²)",
         key="a_e",
         format=num_format,
         disabled=is_disabled("a_e"),
+        on_change=react,
+        kwargs={"symbol": a_e},
     )
 
 st.divider()
 
 st.header("Steps :")
+
+for step in st.session_state['steps'] :
+    st.write(step)
 
 st.header("STATE")
 
